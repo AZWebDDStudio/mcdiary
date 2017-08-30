786e08a6bd4c32e811c8238fc2d34709f9784051
AZWebDDS <azwebdds@gmail.com>
2017-08-30 05:37:28 +0300

    Проект Дневник Кодера для статичного сайта на GitHub
    SAND-mcdiary-v1.loc
    
    Первый коммит в ветку MASTER
    Все файлы

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php 
+echo "<h1>Hello World!</h1>"; 
+echo "<div style='color: green'>Ваш новый локальный хост (сайт) готов!</div>"; 
+echo "<div><a href='http://SAND-mcdiary-v1.loc'>http://SAND-mcdiary-v1.loc</a></div>"; 
+phpinfo(); 
+?> 
